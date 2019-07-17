012bec85f27541521ee2494b726c6834ea40e76d
aynur <aykuli@ya.ru>
2019-07-17 12:52:47 +0700

    2019_07_17_gllasy

--- projects/gllacy/js/script.js
+++ projects/gllacy/js/script.js
@@ -1,17 +1,36 @@
-var sliderItemRuond1 = document.querySelector(".item1-round");
-var sliderItem1 = document.querySelector(".item1");
+let sliderItemRuond1 = document.querySelector(".item1-round");
+let sliderItem1 = document.querySelector(".item1");
 
-var sliderItem2 = document.querySelector(".item2");
-var sliderItemRuond2 = document.querySelector(".item2-round");
+let sliderItem2 = document.querySelector(".item2");
+let sliderItemRuond2 = document.querySelector(".item2-round");
 
-var sliderItem3 = document.querySelector(".item3");
-var sliderItemRuond3 = document.querySelector(".item3-round");
+let sliderItem3 = document.querySelector(".item3");
+let sliderItemRuond3 = document.querySelector(".item3-round");
 
 let bg = document.querySelector(".bg");
 let bgLight = document.querySelector(".bg-light");
 
 let catalogMenu = document.querySelector(".catalog-menu");
 let menuFallOut = document.querySelector(".menu-fall-out");
+
+let search = document.querySelector(".search");
+
+let modalSearch = document.querySelector(".modal-search");
+let modalLogin = document.querySelector(".modal-login");
+let loginLink = document.querySelector(".login-link");
+var login = modalLogin.querySelector("[name=email]");
+var password = modalLogin.querySelector("[name=password]");
+let modalBasket = document.querySelector(".modal-basket");
+let basketNoEmpty = document.querySelector(".basket-no-empty");
+
+let hitSectionLi = document.querySelector(".hit-section-li");
+let hitSectionLi2 = document.querySelector(".hit-section-li2");
+let fast = document.querySelector(".fast");
+
+
+//спрашиваем у браузера, есть ли у него в локальных записях логин пользователя
+let storage = "";
+let isStorageSupport = true;
 
 /*отлов ошибок браузера*/
 try {
@@ -22,20 +41,82 @@ try {
 /*END OF отлов ошибок браузера*/
 
 /*FUNCTIONS*/
-
-
 catalogMenu.addEventListener("mouseenter", function (evt) {
 		console.log("Открытие выпадающего меню Каталог");
 		menuFallOut.classList.remove("display-none");	
 });
-
 catalogMenu.addEventListener("mouseleave", function (evt) {
 		console.log("Закрытие выпадающего меню Каталог");
 		menuFallOut.classList.add("display-none");	
 });
 
 
+search.addEventListener("mouseenter", function (evt) {
+		console.log("Открытие формы для поиска по сайту");
+		modalSearch.classList.remove("display-none");
+});
+search.addEventListener("mouseleave", function (evt) {
+		console.log("Закрытие формы для поиска по сайту");
+		modalSearch.classList.add("display-none");
+});
 
+loginLink.addEventListener("click", function (evt) {
+	evt.preventDefault();
+});
+loginLink.addEventListener("mouseenter", function (evt) {
+	evt.preventDefault();
+	console.log("Открытие формы для авторизации");
+	modalLogin.classList.remove("display-none");
+	if (storage) {
+		email.value = storage;
+		password.focus();
+	} else {
+		login.focus();
+	}
+});
+loginLink.addEventListener("mouseleave", function (evt) {
+	evt.preventDefault();
+	console.log("Открытие формы для авторизации");
+	modalLogin.classList.add("display-none");
+});
+
+
+basketNoEmpty.addEventListener("mouseenter", function (evt) {
+		evt.preventDefault();
+		console.log("Открытие списка корзины");
+		modalBasket.classList.remove("display-none");
+});
+basketNoEmpty.addEventListener("mouseleave", function (evt) {
+		console.log("Закрытие списка корзины");
+		modalBasket.classList.add("display-none");
+});
+
+hitSectionLi.addEventListener("mouseenter", function (evt) {
+		console.log("Подсветка фона хита при наведении");
+		fast.classList.remove("display-none");
+});
+hitSectionLi.addEventListener("mouseleave", function (evt) {
+		console.log("Убрать подсветку фона хита при наведении");
+		fast.classList.add("display-none");
+});
+
+hitSectionLi2.addEventListener("mouseenter", function (evt) {
+		console.log("Подсветка фона хита при наведении");
+		fast.classList.remove("display-none");
+});
+hitSectionLi2.addEventListener("mouseleave", function (evt) {
+		console.log("Убрать подсветку фона хита при наведении");
+		fast.classList.add("display-none");
+});
+
+ /*Убрать окошко по ESC*/
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		modalSearch.classList.add("display-none");
+		modalLogin.classList.add("display-none");
+		} 
+});
+/**/
 
 sliderItemRuond1.addEventListener("click", function (evt) {
 	sliderItemRuond1.classList.add("item-round-after");
