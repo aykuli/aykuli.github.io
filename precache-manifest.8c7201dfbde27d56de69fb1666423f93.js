0981c1c306ddb9a233169bb4cf4f3e4ca6271ed1
Aykuli <aykuli@ya.ru>
2020-01-27 11:13:09 +0700

    feat: add about page

---
+++ precache-manifest.8c7201dfbde27d56de69fb1666423f93.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803b3e7dd6d6db2045165b41fef19116",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0910886922d576306738",
+    "url": "./static/css/main.a0c0437d.chunk.css"
+  },
+  {
+    "revision": "c98964acf12bb6fb8101",
+    "url": "./static/js/2.4cc2005c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.4cc2005c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0910886922d576306738",
+    "url": "./static/js/main.96007782.chunk.js"
+  },
+  {
+    "revision": "5196e3e099fec11c45ff",
+    "url": "./static/js/runtime-main.44af3553.js"
+  },
+  {
+    "revision": "2aea532bb5e7d67efabb6170ddeb3cf4",
+    "url": "./static/media/arrow.2aea532b.svg"
+  },
+  {
+    "revision": "0aed2ac23855a8ff7233b3dcc2a478ab",
+    "url": "./static/media/chuck-norris.0aed2ac2.jpg"
+  },
+  {
+    "revision": "7e47f166bf1afe7ae8895f0e346a5b50",
+    "url": "./static/media/chuck-norris_300x225.7e47f166.jpg"
+  },
+  {
+    "revision": "3830698b506a06fe7c1e843aa6be382f",
+    "url": "./static/media/fancy-weather copy.3830698b.jpg"
+  },
+  {
+    "revision": "c64f95e7bbdb36ddf905004b3dc57367",
+    "url": "./static/media/fancy-weather.c64f95e7.jpg"
+  },
+  {
+    "revision": "ccf734c38efccc4cbd3b0dae35fd54d1",
+    "url": "./static/media/fancy-weather_300x225.ccf734c3.jpg"
+  },
+  {
+    "revision": "0ed1b71b65dfadbe98947373c88baf19",
+    "url": "./static/media/gllasy.0ed1b71b.jpg"
+  },
+  {
+    "revision": "7074dd70761eb8c49d85ba9b66715a1d",
+    "url": "./static/media/gllasy_300x225.7074dd70.jpg"
+  },
+  {
+    "revision": "2add818c22b96b08d80503a2a4a9332f",
+    "url": "./static/media/halbstadt.2add818c.jpg"
+  },
+  {
+    "revision": "f858e0c88996a4633202a181426f286a",
+    "url": "./static/media/halbstadt_300x225.f858e0c8.jpg"
+  },
+  {
+    "revision": "7228d601b6a6d4a7d097db64f0530975",
+    "url": "./static/media/icon-codewars.7228d601.svg"
+  },
+  {
+    "revision": "9bd50a7c05bd6713b0ce278cb1542b4f",
+    "url": "./static/media/icon-email.9bd50a7c.svg"
+  },
+  {
+    "revision": "e4eff350c5565d82fbd5e6550a39afae",
+    "url": "./static/media/icon-github.e4eff350.svg"
+  },
+  {
+    "revision": "0ed81885b373d5d690bad295a17ae108",
+    "url": "./static/media/image-api.0ed81885.jpg"
+  },
+  {
+    "revision": "672026bc547b051703d96de5486e8d23",
+    "url": "./static/media/image-api_300x225.672026bc.jpg"
+  },
+  {
+    "revision": "5b748aa2292dc28f2407b544b94bd7fe",
+    "url": "./static/media/meme-generator.5b748aa2.jpg"
+  },
+  {
+    "revision": "5227fb8a0977cb39a28e419776cb8c79",
+    "url": "./static/media/meme-generator_300x225.5227fb8a.jpg"
+  },
+  {
+    "revision": "f34ed3492633bd06e9ea2812fab3acac",
+    "url": "./static/media/piskel-clone.f34ed349.jpg"
+  },
+  {
+    "revision": "defb0c1b50053c2d58a34d716ba6e4d4",
+    "url": "./static/media/piskel-clone_300x225.defb0c1b.jpg"
+  },
+  {
+    "revision": "d85d5675ce34c528a7dc75d8a8322611",
+    "url": "./static/media/rectangle.d85d5675.svg"
+  },
+  {
+    "revision": "c71f542cfd02f1fe6feccc575195d963",
+    "url": "./static/media/repair-design-project.c71f542c.jpg"
+  },
+  {
+    "revision": "0cd6a1ef8075b63741131ebfcefa40e9",
+    "url": "./static/media/repair-design-project_300x225.0cd6a1ef.jpg"
+  },
+  {
+    "revision": "fa71657a5c42c69b208c1148f956d8af",
+    "url": "./static/media/todo-redux.fa71657a.jpg"
+  },
+  {
+    "revision": "736143b5de682b5a6df53964ddbbd721",
+    "url": "./static/media/virtual-keyboard.736143b5.jpg"
+  },
+  {
+    "revision": "48e7c756100e31f4247ee40515302e37",
+    "url": "./static/media/virtual-keyboard_300x225.48e7c756.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b8b0a378733118971eb89eefce227086.js"
+  "./precache-manifest.8c7201dfbde27d56de69fb1666423f93.js"
 );
 
 self.addEventListener('message', (event) => {
