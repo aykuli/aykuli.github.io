838802f11abcd88a755f71157385d34d937f9177
aynur <aykuli@ya.ru>
2022-12-20 18:14:24 +0700

    piskel-clone in gh-pages

--- src/components/drawCanvas/clearCanvas.js
+++
@@ -1,7 +0,0 @@
-export default function clearCanvas(canvas, piskelImg, currentCount, imgsKey) {
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  piskelImg[currentCount] = ''; // eslint-disable-line
-  localStorage.removeItem(imgsKey);
-  localStorage.setItem(imgsKey, JSON.stringify(piskelImg));
-}
--- src/components/tools/swapColors.js
+++
@@ -1,7 +0,0 @@
-export default function swapHandler(primaryColor, secondaryColor, ctx) {
-  const buf = primaryColor.value;
-  primaryColor.value = secondaryColor.value; // eslint-disable-line
-  secondaryColor.value = buf; // eslint-disable-line
-  ctx.fillStyle = primaryColor.value;
-  return [primaryColor.value, secondaryColor.value];
-}
